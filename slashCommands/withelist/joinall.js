6d3eb98c2a57d18b595886a42c9b3d1f050be8f7
uruedosu <107955891+uruedosu@users.noreply.github.com>
2023-05-29 17:46:16 +0000

    Add files via upload

--- slashCommands/withelist/joinall.js
+++ slashCommands/withelist/joinall.js
@@ -1,9 +1,9 @@
-const discord = require('discord.js');
+const discord = require('discord.js')
 const users = require('../../models/users');
 
 module.exports = {
   name: "joinall",
-  description: "🏃 Veritabanındaki tüm kullanıcıları sunucuya katılıyor",
+  description: "🏃 joins all people in db",
   default_permission: true,
   timeout: 3000,
   category: "whitelist",
@@ -11,35 +11,32 @@ module.exports = {
   ownerOnly: false,
 
   run: async (client, interaction, args) => {
+
     const data = await users.find();
     let error = 0;
     let success = 0;
-    let alreadyJoined = 0;
-
-    await interaction.reply(`**Kullanıcılar katılıyor...** \`0\`/\`${data.length}\``);
+    let already_joined = 0;
+    await interaction.reply(`**Users joining...** \`0\`/\`${data.length}\``)
     const inter = setInterval(async () => {
-      interaction.editReply(`**Kullanıcılar Katılıyor...** \`${success}\`/\`${data.length}\``);
-    }, 1000);
+      interaction.editReply(`**Users Joining...** \`${success}\`/\`${data.length}\``)
+    }, 1000)
 
     for (const i of data) {
-      const user = await client.users.fetch(i.userId).catch(() => {});
-      if (interaction.guild.members.cache.get(i.userId)) {
-        alreadyJoined++;
-      } else {
-        await interaction.guild.members.add(user, { accessToken: i.accessToken }).catch(() => {
-          error++;
-        });
-        success++;
-      }
+        const user = await client.users.fetch(i.userId).catch(() => { });
+        if (interaction.guild.members.cache.get(i.userId)) {
+          already_joined++
+        } else {
+          await interaction.guild.members.add(user, { accessToken: i.accessToken }).catch(() => {
+            error++
+          })
+          success++ 
+        }
     }
-
-    clearInterval(inter);
-    await interaction.editReply({
-      embeds: [{
-        title: `${client.user.username} > Joinall`,
-        description: `**Sunucuda Zaten Bulunan Üyeler**: ${alreadyJoined}\n**Başarılı**: ${success}\n**Hata**: ${error}`,
-        color: "2F3136"
-      }]
-    });
+    await clearInterval(inter)
+    await interaction.editReply({ embeds: [{
+          title: `${client.user.username} > Joinall`,
+          description: `**members allready in server**: ${already_joined}\n**Succes**: ${success}\n**error**: ${error}`,
+          color: "2F3136"
+        }]})
   }
-};
+}
