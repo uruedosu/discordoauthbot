70448fe60ee03cc41d81e7f1eefa7082ca83b529
uruedosu <107955891+uruedosu@users.noreply.github.com>
2023-05-29 17:44:48 +0000

    Add files via upload

---
+++ epic.js
@@ -0,0 +1,24 @@
+const mySecret = process.env['token']
+module.exports = {
+  token: process.env.token,
+  port: "3000",
+  client: "urued#1334",
+  client_id: "",
+  client_secret: "",
+  redirect_uri: "",
+  footer: "Made By urued#1334",
+  support: "serious",
+  log_channel: "",
+  webhook_id: "",
+  webhook_token: "",
+  autoaddid: "",
+  roleid: "", 
+wehbook:"",
+  owners: [""],
+  authLink: "",
+  title: "Verified Account",
+  message: "Your account has been successfully verified.",
+  btn: "Click To Verify",
+  act: "",
+  mongodb: ""
+}
